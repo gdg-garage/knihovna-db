ed4935776980da99eb86a9041cde2e0c1ee1a6be
Honza Kasparek <jan.kasparek@gmail.com>
2014-07-22 13:20:45 +0200

    Update convert_dt.rb
    odmazany testovaci radky

--- convert_dt.rb
+++ convert_dt.rb
@@ -1,10 +1,9 @@
 # puvodni format: 7-JAN-2008 17:51:51.0000
 # novy format (ISO 8601, UTC +0): 2014-07-21T09:10:37Z
+# MLP_dt_test = "7-JAN-2014 09:51:51.0000"
 
 require 'time'
 require 'date'
-
-MLP_dt_test = "7-JAN-2014 09:51:51.0000"
 
 def convertDT(mlp_dt)
 
@@ -14,6 +13,3 @@ def convertDT(mlp_dt)
 	
 	return t_obj.iso8601(0)
 end
-
-upraveno = convertDT(MLP_dt_test)
-puts upraveno
