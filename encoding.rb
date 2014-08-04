13012b7fba24b93c762e01f98f6055f3633e6954
radim.kliment <radim.kliment@clevertech.cz>
2014-08-04 20:06:36 +0200

    adding convert encoding map

---
+++ encoding.rb
@@ -0,0 +1,16 @@
+char_map=[
+	{230,"Š"},{160,"á"},{236,"ý"},
+	{133,"ů"},{159,"č"},{161,"í"},
+	{130,"é"},{216,"ě"},{253,"ř"}, 
+	{156,"ť"},{144,"É"},{231,"š"},
+	{160,"á"}]
+
+pangram = "Příliš žluťoučký kůň úpěl ďábelské ódy"
+
+def convert_to_utf8(string)
+	string.each_char do |char|
+		puts char.ord
+	end
+end
+
+convert_to_utf8("a")
