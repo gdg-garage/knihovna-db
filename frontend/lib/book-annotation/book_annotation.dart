bba9204c1cc258e388c288c5d2ddca61fa985da2
Filip Hracek <filiph@google.com>
2014-11-20 16:43:01 +0100

    Add book-annotation stub

---
+++ frontend/lib/book-annotation/book_annotation.dart
@@ -0,0 +1,12 @@
+library book_annotation;
+
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:async';
+import 'package:core_elements/core_ajax_dart.dart';
+import 'dart:convert';
+
+@CustomTag('book-annotation')
+class BookAnnotation extends PolymerElement {
+  BookAnnotation.created() : super.created();
+}
